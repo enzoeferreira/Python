c53386e5991ad4dd44cf527560af4799d785fe6e
enzoeferreira <enzoeferreira@gmail.com>
2022-08-25 15:05:24 -0300

    [MODIFIED] added arredondamento da pressao

--- MC102/08.25.22/calculo_pressao.py
+++ MC102/08.25.22/calculo_pressao.py
@@ -6,7 +6,9 @@ pi = 3.14159265 # Pi constante, é possível colocar uma biblioteca matemática
 area = pi * (raio ** 2) # Calcula a àrea (pi)r²
 pressao = forca / area  # Calcula a pressão
 
-print(pressao, "Pa")    # Printa a pressão no formato "X Pa"
+print(f"{pressao:.2f} Pa") # Printa a pressão no formato "X Pa" com 2 casas decimais
+
+# print(pressao, "Pa")    # Printa a pressão no formato "X Pa"
 
 """
 Outra forma de printar seria
