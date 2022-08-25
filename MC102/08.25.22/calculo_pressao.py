52de8915d0ec76bca86cdf03854b5a3d19647221
enzoeferreira <enzoeferreira@gmail.com>
2022-08-25 14:40:31 -0300

    [MODIFIED] added comentários

--- MC102/08.25.22/calculo_pressao.py
+++ MC102/08.25.22/calculo_pressao.py
@@ -1,12 +1,12 @@
-forca = float(input())
-raio = float(input())
+forca = float(input())  # Recebe o número racional da força
+raio = float(input())   # Recebe o número racional do raio
 
-pi = 3.14159265
+pi = 3.14159265 # Pi constante, é possível colocar uma biblioteca matemática
 
-area = pi * (raio ** 2)
-pressao = forca / area
+area = pi * (raio ** 2) # Calcula a àrea (pi)r²
+pressao = forca / area  # Calcula a pressão
 
-print(pressao, "Pa")
+print(pressao, "Pa")    # Printa a pressão no formato "X Pa"
 
 """
 Outra forma de printar seria
