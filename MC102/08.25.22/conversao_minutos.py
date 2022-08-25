920634f3fe9f0f01b81e970cec93dda0a416d6c4
enzoeferreira <enzoeferreira@gmail.com>
2022-08-25 14:34:57 -0300

    [MODIFIED] added comentarios

--- MC102/08.25.22/conversao_minutos.py
+++ MC102/08.25.22/conversao_minutos.py
@@ -1,11 +1,11 @@
 from tkinter import N
 
 
-n = int(input())
-horas = n // 60
-minutos = n % 60
+n = int(input())    # Recebe a quantidade de minutos
+horas = n // 60     # Divisão inteira de minutos por 60
+minutos = n % 60    # Resto da divisão inteira
 
-print(horas, "h", minutos, "min", sep="")
+print(horas, "h", minutos, "min", sep="") # Printa o tempo em horas/minutos no formato XhYmin
 
 """"
 Outra forma de printar seria
