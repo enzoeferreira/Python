9c604bac95f2b6ca1b6a12a5480d74380c074d53
enzoeferreira <enzoeferreira@gmail.com>
2022-09-08 11:27:09 -0300

    [ADDED] maior_2_numeros.py

---
+++ MC102/09.06.22/maior_2_numeros.py
@@ -0,0 +1,11 @@
+# Pode ser simplificado com a função max()
+
+x = float(input("Digite um número: "))
+y = float(input("Digite outro número: "))
+
+if x > y:
+    maior = x
+else:
+    maior = y
+    
+print("Maior =", maior)
